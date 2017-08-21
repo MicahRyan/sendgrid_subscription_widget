e73f6d27d538ccb3d97d8a4309ad4966ac0270a6
MicahRyan <contact@micahryan.com>
2017-08-21 09:40:40 -0500

    Update App Settings
    Update Your App Settings in Your Forked Repository on Github

--- settings.js
+++ settings.js
@@ -1,16 +1,16 @@
 // Change the url to the domain of your app
-exports.url = 'http://localhost:3090';
+exports.url = 'https://kingsseafood-subscription.herokuapp.com/';
 
 exports.senderEmail = "sender@example.com";
 exports.senderName = "Sender Name";
 
 // set 'exports.listId = null' to add contact to all contacts, but no specific list
 // or a string with the listId to add to a specific list
-exports.listId = null;
+exports.listId = 1480985;
 
 // set 'exports.templateId = null' to opt out of using a template
 // or a string with the templateId to use a template
-exports.templateId = null;
+exports.templateId = e025c874-646b-4aa3-8369-e86c581b12bb;
 
 // receive an email when a new signup is confirmed
 exports.sendNotification = true;
